5e40eab7d83012cf215aefaba4b7be4ddd4ba416
Sarthak <sgrover@protonmail.ch>
2018-12-26 03:11:39 +0530

    class CDN to find cdn using all methods and save relevant data to pickle and pandas

--- estimate_cdn.py
+++ estimate_cdn.py
@@ -1,26 +1,60 @@
-import pandas as pd
-from find_cdn_methods import *
-
-from CDNdomains import cdn_domains
+from __future__ import division
 import urllib.parse
 from bs4 import BeautifulSoup
 from collections import defaultdict, Counter
+import os, re
+import pickle as pkl
+import pandas as pd
 
-site_cdn_method_map = defaultdict(list)
+import parse_whois
+from CDNdomains import cdn_domains, cdn_names
+
+
+def split_before(pattern, text):
+    prev = 0
+    for m in re.finditer(pattern,text):
+        yield text[prev:m.start()]
+        prev = m.start()
+    yield text[prev:]
 
 
 class CDN:
     def __init__(self, rank, site):
         self.rank = rank
         self.site = site
-        self.cdn = None
-
-        self.url = "https://www."+site
+        self.url = "https://www." + site
         self.object_netlocs = []
         self.num_links = 0
         self.object_cdn = defaultdict(int)
         self.object_unknown = defaultdict(int)
 
+        self.cdn_by_site = self._find_cdn_by_site(self.site)
+        self.cdn_by_whois = self._find_cdn_by_whois_org(self.site)
+        self.cdn_by_cname = None  # TODO find if dns.cname points to a CDN url
+        self.cdn_by_xcache = None  # TODO find 'x-cache: HIT or MISS in headers and match to CDN name
+
+        self.cdn_by_parse = None
+        self.cdn_score = {}
+        self.most_linked_url = None
+
+        self.cdn = None
+
+    def estimate_cdn(self):
+        """
+        final_cdn priority is by:
+        - xcache: if objects on page were on cdn, xcache shows hit or miss and also cdn name.
+            - Can also be used to populate url list on the fly as analysis progresses
+            - #TODO NOT IMPLEMENTED YET
+        - site: compare site name against "list of well known cdn urls".
+        - cname: returned cname is cdn url then original site is getting redirected to "list of well known cdn urls"
+            -#TODO
+        - parse: compare object links on page to "list of well known cdn urls"
+
+        - whois: not too reliable, returns information on owner of IP range rather than CDN directly.
+            - Works for most CDN providers, but requires maintaining list of famous CDN names
+            - Fails if provider sells hosting services and CDN (eg Amazon AWS and Cloudfront are both on same ASN)
+        """
+        return self.cdn_by_xcache or self.cdn_by_site or self.cdn_by_cname or self.cdn_by_parse
 
     @staticmethod
     def _get_parsed_info(loc):
@@ -30,6 +64,31 @@ class CDN:
         parsed = urllib.parse.urlparse(loc)
         fname, fextension = os.path.splitext(parsed.path)
         return fextension, parsed.netloc
+
+    @staticmethod
+    def _find_cdn_by_site(site):
+        """
+        input site and cdn_domains dict
+        search site in cdn_domains top down
+        return cdn_name
+        """
+        for cdn_url in cdn_domains.keys():
+
+            url = "." + site
+            if cdn_url in url:
+                #print("\t\t\t\t\t" + cdn_url + " in " + site)
+                return cdn_domains[cdn_url]
+
+            if site in cdn_url:
+                # for short sites like t.co that are in all cdn_domains
+                check_prefix, check_suffix = cdn_url.split(site,
+                                                           1)  # split only once starting from left, atleast . prefix
+                if (check_prefix[-1] == ".") or (check_prefix[-4:] == '.cdn'):
+                    # only if site was complete, last char of prefix will be a '.' from cdn_domains
+                    #print("\t\t\t\t\t"+site+" in "+cdn_url)
+                    return cdn_domains[cdn_url]
+                # print("\t\t\t\t\tError checking "+site+" in "+cdn_url)
+        return None
 
     @staticmethod
     def _find_cdn_by_url(url):
@@ -48,6 +107,50 @@ class CDN:
             if cdn_url in url:
                 return cdn_domains[cdn_url]
         return False
+
+    @staticmethod
+    def _find_cdn_by_whois_org(site):
+        # TODO use difflib.SequenceMatcher to select best match instead of exact string matches
+
+        whois = parse_whois.loadwhoisIP(site)
+        Org = parse_whois.searchOrg(whois)
+
+        for org in Org:
+            for cdn in cdn_names:
+                if (org.lower() in cdn.lower()) or (cdn.lower() in org.lower()):
+                    return cdn
+            for cdn in cdn_domains.values():
+                if (org.lower() in cdn.lower()) or (cdn.lower() in org.lower()):
+                    return cdn
+        return None
+
+    @staticmethod
+    def _get_cdn_url(url):
+        if "cdn" in url:
+            x = list(split_before("cdn", url))
+            if x[0] == "":
+                cdn_url = ''.join(x)
+            else:
+                cdn_url = x[0].split(".")[-1] + ''.join(x[1:])
+            return cdn_url
+        else:
+            return url
+
+    def get_cdn_score(self, cdn):
+        if cdn is None:
+            return 0
+        return self.cdn_score[cdn]
+
+    def get_total_links(self):
+        return self.num_links
+
+    def set_cdn_by_site(self):
+        self.cdn_by_site = self._find_cdn_by_site(self.site)
+        return
+
+    def set_cdn_by_whois(self):
+        self.cdn_by_whois = self._find_cdn_by_whois_org(self.site)
+        return
 
     def count_netlocs(self, data):
         """find static resources on a webpage data and urls they are served from"""
@@ -108,133 +211,90 @@ class CDN:
                     if cdn:
                         self.object_cdn[cdn] += 1
                     elif "cdn" in url:
-                        self.object_cdn[url] += 1
+                        cdn_url = self._get_cdn_url(url)
+                        self.object_cdn[cdn_url] += 1
                     else:
                         self.object_cdn["unknown"] += 1
                         self.object_unknown[url] += 1
         return
 
-    def estimate_cdn(self):
+    def estimate_cdn_by_parse(self):
+        """
+        sort object_cdn by their count and relative ratio
+        if there are at least 5 total external links, and link with highest ratio has majority
+        set it as cdn_by_parse name
+        """
         sorted_cdn_count = sorted(self.object_cdn.items(), key=lambda value: value[1], reverse=True)
-        print(self.site, self.num_links, sorted_cdn_count)
+        self.cdn_score = {tup[0]:tup[1]/self.get_total_links() for tup in sorted_cdn_count}
+
         if len(sorted_cdn_count) == 0:
-            return None
-        return sorted_cdn_count[0]
+            self.cdn_by_parse = None
+            return
+
+        likely_cdn_name = sorted_cdn_count[0][0]
+
+        if (likely_cdn_name is not None) and (likely_cdn_name != "unknown"):
+            if self.get_total_links() > 4:      # at least 5 external urls
+                cdn_score = self.get_cdn_score(likely_cdn_name)
+                if cdn_score >= 0.5:
+                    self.cdn_by_parse = likely_cdn_name
+
+        if likely_cdn_name == "unknown":
+            sorted_url_count = sorted(self.object_unknown.items(), key=lambda value: value[1], reverse=True)
+            if self.get_total_links() > 4:
+                url_score = sorted_url_count[0][1]/self.get_total_links()
+                if url_score >= 0.5:
+                    self.most_linked_url = sorted_url_count[0][0]
+        return
 
     def process(self):
         cnt = self.get_homepage_url_count()
-        #print(cnt)
         self.object_url_to_cdn()
+        self.estimate_cdn_by_parse()
         self.cdn = self.estimate_cdn()
-
         return
 
 
-def cdn_parse_site(site):
-    """wrapper func parsing site name and urls on home page"""
-    # site in cdn or cdn in site urls
-    matching_cdn = find_cdn_by_site(site) or find_cdn_by_url(site) or find_cdn_by_counting_url(site)
-
-    if not matching_cdn:  # False or "" are both returned as False
-        return False
-
-    return matching_cdn
-
-
-def cdn_parse_whois(site):
-    """wrapper func calling whois"""
-    # whois org is cdn
-    matching_cdn = find_cdn_by_whois_org(site)
-
-    if not matching_cdn:  # False or "" are both returned as False
-        # matching_cdn = find_cdn_by_whois_site(site)
-        return False
-
-    return matching_cdn
-
-
-def estimate_cdn(site, cdn_parsed, cdn_whois, prioritize_whois=False):
-    """logic to decide which CDN finally - prioritizes parsed based detection instead of whois records
-    if prioritize_whois is set to True, then if cdn_whois is a famous well known CDN, it is returned"""
-
-    if prioritize_whois and cdn_whois in major_cdns:
-        # in case ip has this whois, regardless of static resources return this info
-        # usually set to false since we want to prioritize static resources based CDN over whois data
-        matching_cdn = cdn_whois
-
-    elif cdn_parsed and cdn_whois:  # confusion case when both exist
-        if cdn_parsed in all_cdn_names:
-            # known CDN not an unknown url
-            matching_cdn = cdn_parsed
-        else:
-            matching_cdn = cdn_whois
-            # note association between cdn_parsed of url type and cdn_whois
-            global site_cdn_method_map
-            site_cdn_method_map['site'].append(site)
-            site_cdn_method_map['cdn_parsed'].append(cdn_parsed)
-            site_cdn_method_map['cdn_whois'].append(cdn_whois)
-
-    else:
-        # assume cdn_whois has higher priority if cdn_parsed is an unknown cdn url - most will be amazon aws
-        matching_cdn = cdn_whois or cdn_parsed
-
-    """
-    global i
-    if cdn_parsed and cdn_whois:
-        if cdn_parsed != cdn_whois:
-            i += 1
-            print(i, ". ", site, ":", matching_cdn, "<", cdn_parsed, "|", cdn_whois, ">")
-    """
-
-    return matching_cdn
-
-
 def main():
-
-    print("Reading file: 'output/df_data.json'\n")
-    df_data = pd.read_json('output/df_data.json')
-    df_valid = df_data[df_data['ip']!=False].copy()
-
-    print("Number of sites loaded: %s" % (len(df_data)))
-    print("Number of valid sites for further analysis: %s\n" % (len(df_valid)))
-    print("List of blocked sites: %s" % list(df_data[df_data['ip']==False]['site']))
-
-    df_valid['cdn_parsed'] = df_valid['site'].apply(cdn_parse_site)
-    df_valid['cdn_whois'] = df_valid['site'].apply(cdn_parse_whois)
-    df_valid['cdn'] = False
-
-    df_valid['cdn'] = df_valid.apply(lambda row:
-                                     estimate_cdn(row['site'], row['cdn_parsed'], row['cdn_whois'], False),
-                                     axis=1)
-
-    df_url_cdn_whois_map = pd.DataFrame(site_cdn_method_map)
-    df_url_cdn_whois_map.to_pickle('output/df_url_cdn_whois_map.pkl')
-
-    df_valid.to_pickle('output/df_ip_asn_cdn.pkl')
-
-    print("\n\nCreating pandas dataframe file: 'output/df_url_cdn_whois_map.pkl")
-    print("Creating pandas dataframe file: 'output/df_ip_asn_cdn.pkl")
-
-    return
-
-
-def main2():
     from fetch_site_info import load_sites
-    rank_to_site = load_sites("data/top-1m-new.csv", 10)
+    nwebsites = 500
+
+    rank_to_site = load_sites("data/top-1m-new.csv", )
     cdn = {}
+    data = defaultdict(list)
 
     for rank, site in rank_to_site.items():
-        #print(rank, site)
         c = CDN(rank, site)
         c.process()
         cdn[rank] = c
-        print(c.object_unknown)
+        print(rank, site, c.cdn, c.cdn_by_site, c.cdn_by_whois, c.cdn_by_parse, c.get_total_links(),
+              c.get_cdn_score(c.cdn_by_parse), c.most_linked_url, sep="|")
+
+        # To store in pandas dataframe
+        data['rank'].append(c.rank)
+        data['site'].append(c.site)
+        data['cdn'].append(c.cdn)
+        data['cdn.site'].append(c.cdn_by_site)
+        data['cdn.parse'].append(c.cdn_by_parse)
+        data['cdn.most_url'].append(c.most_linked_url)
+        data['cdn.whois'].append(c.cdn_by_whois)
+        data['cdn.xcache'].append(c.cdn_by_xcache)
+        data['cdn.cname'].append(c.cdn_by_cname)
+
+    foutpath = "results/CDN_data_object.pkl"
+    print("Save CDN class data for top %s sites to %s" %(nwebsites, foutpath))
+    pkl.dump(cdn, open(foutpath, "wb"))
+
+    df_cdn = pd.DataFrame(data)
+    foutpath2 = "results/df_cdn.pkl"
+    print("Save pandas dataframe with site, cdn using all available methods to %s" % (foutpath2))
+    df_cdn.to_pickle(foutpath2)
 
     return
 
 
 if __name__ == '__main__':
 
-    main2()
+    main()
 
 
